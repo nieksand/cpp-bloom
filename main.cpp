f7fab4501e21862c8738a74fdf72e4a7f69f3021
Niek Sanders <niek.sanders@gmail.com>
2012-06-02 17:45:27 -0700

    Tweaked demo parameters.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,27 @@
 #include "BloomFilter.h"
 
 
+/** 
+ * Main program.
+ * 
+ * \param    argc   Number of command line arguments.
+ * \param    argv   Text of command line arguments.
+ * 
+ * \return   zero if successful; nonzero otherwise.
+ */
 int 
 main( int argc, char* argv[] ) {
 
-    BloomFilter<1000> bf( 500 );
+    BloomFilter<800> bf( 100 );
 
-    bf.addElement( 123 );
+    // add in elements
+    int j =0 ;
+    for ( int i = 0; i < 1000; i += 10 ) {
+        bf.addElement( i );
+    }
 
-    for ( int i = 0; i < 1000000; ++i ) {
+    // look at both real hits and false positives
+    for ( int i = 0; i < 2000; ++i ) {
         if ( bf.containsElement( i ) ) {
             std::cout << "match -> " << i << std::endl;
         }
